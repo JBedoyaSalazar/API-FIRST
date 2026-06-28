06260ce9d10bed403062318149f113b03f354d0b
Jefred Bedoya <jefred03salazar@gmail.com>
2026-06-28 13:40:05 -0500

    feat: implement express server with OpenAPI validation and documentation routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import express from "express";
 import YAML from "yamljs";
 import swaggerUi from "swagger-ui-express";
+import OpenApiValidator from 'express-openapi-validator';
 
 const app = express();
 const port = 3000;
 
 const swaggerDocument = YAML.load("./openapi.yaml");
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(OpenApiValidator.middleware({
+    apiSpec: swaggerDocument,
+    validateRequests: true,
+    validateResponses: true,
+    ignorePaths: /.*\/docs/
+}));
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message,
+        code: err.code || "INTERNAL_ERROR",
+        errors: err.errors
+    });
+});
 
 
 app.get("/hello", (req, res) => {
